b74be68648bcffa71e23ae6b67eb36e7a87a6aa9
MartinL-no <martin.lorimer@icloud.com>
2023-04-08 14:36:26 +0200

    Add project files.

---
+++ LeftHandedMan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WavingMan
+{
+    public class LeftHandedMan : Man
+    {
+        public LeftHandedMan(int x, int y, int dx, int dy) : base(x, y, dx, dy) { }
+
+        public override void Show()
+        {
+            SetCursorTop();
+            SetCursorLeft();
+            Console.WriteLine(_shouldWaveNextTime ? "\\o " : " o");
+            SetCursorLeft();
+            Console.WriteLine(_shouldWaveNextTime ? " |\\ " : " |");
+            SetCursorLeft();
+            Console.Write("/ \\");
+            _shouldWaveNextTime = !_shouldWaveNextTime;
+        }
+
+    }
+}
---
+++ Man.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace WavingMan
+{
+    /*
+     * Oppgave:
+     *  Some wavers are left-handed.
+     *  Rewrite the code to use inheritance instead of bool IsLeftHanded
+     */
+    public class Man
+    {
+        public Position Position { get; }
+        public Position Speed { get; }
+        public bool IsLeftHanded { get; set; }
+        protected bool _shouldWaveNextTime = false;
+
+        public Man(int x, int y, int dx, int dy)
+        {
+
+            Position = new Position(x, y);
+            Speed = new Position(dx, dy);
+        }
+
+        public virtual void Show()
+        {
+            SetCursorTop();
+            SetCursorLeft();
+            Console.WriteLine(_shouldWaveNextTime ? " o/" : " o");
+            SetCursorLeft();
+            Console.WriteLine(_shouldWaveNextTime ? "/|" : " |");
+            SetCursorLeft();
+            Console.Write("/ \\");
+            _shouldWaveNextTime = !_shouldWaveNextTime;
+        }
+
+        protected void SetCursorTop()
+        {
+            Console.CursorTop = Math.Max(Position.Y, 0);
+        }
+
+        protected void SetCursorLeft()
+        {
+            Console.CursorLeft = Math.Max(Position.X - 1, 0);
+        }
+
+        public void Move()
+        {
+            var windowWidth = Console.WindowWidth;
+            var windowHeight = Console.WindowHeight;
+
+            if (Position.X <= 1) Speed.X = 1;
+            if (Position.X >= windowWidth - 1) Speed.X = -1;
+            if (Position.Y <= 1) Speed.Y = 1;
+            if (Position.Y >= windowHeight - 1) Speed.Y = -1;
+
+            Position.Move(Speed);
+        }
+    }
+}
